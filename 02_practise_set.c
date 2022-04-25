563a30fbbd7bca9b199d1c135eacbfb186904e58
Akassshhh <98277745+Akassshhh@users.noreply.github.com>
2022-04-25 17:41:45 +0530

    Program to tell if pass or fail

---
+++ 02_practise_set.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+     int physics,maths,chemistry;
+     float total;
+     printf("Enter physics marks:\n");
+     scanf("%d",&physics);
+
+     printf("Enter maths marks:\n");
+     scanf("%d",&maths);
+
+     printf("Enter chemistry marks:\n");
+     scanf("%d",&chemistry);
+     total = (physics + maths + chemistry)/3;
+
+if(total<40 || physics<33 || maths<33 || chemistry<33){
+    printf("Your marks are %f and you are fail\n",total);
+}
+else{
+    printf("Your marks are %f and you are pass\n",total);
+}
+return 0;
+}
